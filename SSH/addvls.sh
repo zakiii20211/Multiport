010635fd591be88f99b82ce8017a80359f5a646b
zakiii20211 <zakiii20211@gmail.com>
2025-11-04 01:50:42 +0800

    Update addvls.sh

--- SSH/addvls.sh
+++ SSH/addvls.sh
@@ -37,35 +37,6 @@ export TIME="10"
 export URL="https://api.telegram.org/bot$KEY/sendMessage"
 clear
 #IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
-echo -e "\e[32mloading...\e[0m"
-clear
-# Valid Script
-ipsaya=$(curl -sS ipv4.icanhazip.com)
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/KhaiVpn767/multiport/main/setup2.sh"
-checking_sc() {
-  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-  if [[ $date_list < $useexp ]]; then
-    echo -ne
-  else
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${RED}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      ${GREEN}Telegram${NC} https://t.me/khaivpn"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    sleep 5
-    reboot
-  fi
-}
-checking_sc
-echo -e "\e[32mloading...\e[0m"
 clear
 PUB=$(cat /etc/slowdns/server.pub)
 NS=$(cat /etc/xray/dns)
